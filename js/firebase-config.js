9967ff0135567a456c9bb633b3ef214348465b37
jjoy2025 <joydeep2511995@gmail.com>
2025-08-10 17:06:16 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,23 @@
+// Firebase ইম্পোর্ট
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
 
+// আপনার Firebase কনফিগারেশন
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyArpsy4tpySdQEEuZzIa0ZWpz5VzdN7i_I",
+  authDomain: "the-lucky-number-9f211.firebaseapp.com",
+  projectId: "the-lucky-number-9f211",
+  storageBucket: "the-lucky-number-9f211.firebasestorage.app",
+  messagingSenderId: "988704918151",
+  appId: "1:988704918151:web:870a814558135b3c37c622",
+  measurementId: "G-5LL375YSZT"
 };
 
+// Firebase ইনিশিয়ালাইজ
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
+// এক্সপোর্ট
 export { db, auth };
