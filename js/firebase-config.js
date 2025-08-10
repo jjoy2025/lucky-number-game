92a5f3381af59f665c6a85b9301eed6b37d927b4
jjoy2025 <joydeep2511995@gmail.com>
2025-08-10 17:44:48 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,23 +1,44 @@
-// Firebase ইম্পোর্ট
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
+import { 
+  getFirestore, 
+  collection, 
+  doc, 
+  getDoc, 
+  setDoc, 
+  updateDoc,
+  addDoc 
+} from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
+import { 
+  getAuth, 
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
 
-// আপনার Firebase কনফিগারেশন
 const firebaseConfig = {
   apiKey: "AIzaSyArpsy4tpySdQEEuZzIa0ZWpz5VzdN7i_I",
   authDomain: "the-lucky-number-9f211.firebaseapp.com",
   projectId: "the-lucky-number-9f211",
-  storageBucket: "the-lucky-number-9f211.firebasestorage.app",
+  storageBucket: "the-lucky-number-9f211.appbasestorage.app",
   messagingSenderId: "988704918151",
   appId: "1:988704918151:web:870a814558135b3c37c622",
   measurementId: "G-5LL375YSZT"
 };
 
-// Firebase ইনিশিয়ালাইজ
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// এক্সপোর্ট
-export { db, auth };
+export { 
+  db, 
+  auth, 
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  addDoc,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+};
